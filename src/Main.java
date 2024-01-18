6cd04d76cc27e3a5778d36f7a68902835eab2add
Miwwo <miwwoHG@gmail.com>
2024-01-18 20:02:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+
+class Solution {
+    // you need treat n as an unsigned value
+    public static int reverseBits(int n) {
+        StringBuilder sb = new StringBuilder(Integer.toBinaryString(n));
+        sb.reverse();
+        while (sb.length() != 32) sb.append('0');
+        return Integer.parseUnsignedInt(String.valueOf(sb),2);
+    }
+    public static int reverseBitsbyBits(int n) {
+        if (n == 0) return 0;
+
+        int result = 0;
+        for(int i=0; i<32; i++){
+            result <<= 1;
+            result += n&1;
+            n >>= 1;
+        }
+        return result;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Solution.reverseBitsbyBits(43261596));
+    }
+}
